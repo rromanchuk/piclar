01f0a9561c46dcd2c90302c81433ac7354d2e294
Lazarev Ivan <ivan.lazarev@ostrovok.ru>
2012-07-02 18:42:12 +0400

    add registration api

--- web/api/views.py
+++ web/api/views.py
@@ -1,11 +1,17 @@
 import logging
 log = logging.getLogger('web')
 
+from operator import contains
 from tastypie.authorization import Authorization
 from tastypie.authentication import Authentication
-
 from tastypie.resources import ModelResource
+from tastypie.validation import Validation
+from tastypie.exceptions import NotFound
+
 from person.models import Person
+
+
+
 
 class PersonResource(ModelResource):
     class Meta:
@@ -13,8 +19,29 @@ class PersonResource(ModelResource):
         authentication = Authentication()
         authorization = Authorization()
 
+    def _check_field_list(self, bundle, required_fields):
+        return all(
+                map(
+                    lambda x: x in bundle.data, required_fields
+                )
+            )
     def obj_create(self, bundle, request=None):
         # simple registration
-        log.info(bundle.obj)
-        log.info(bundle.data)
+        simple_fields = (
+            'firstname', 'lastname', 'email', 'password',
+        )
+
+        vk_fields = (
+            'email', 'access_token'
+        )
+
+        # is simple registration
+        if self._check_field_list(bundle, simple_fields):
+            bundle.obj.register_simple(**bundle.data)
+        elif self._check_field_list(bundle, vk_fields):
+            bundle.obj.register_vk(**bundle.data)
+        else:
+            raise NotFound('Registration with args [%s] not implemented' %
+               (', ').join(bundle.data.keys())
+            )
         return bundle
