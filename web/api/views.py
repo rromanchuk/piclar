bd89a0769db15d05ec2232c2756b5f463a3f62f8
Lazarev Ivan <ivan.lazarev@ostrovok.ru>
2012-07-02 19:42:43 +0400

    add authorization requirement and handle for logged user

--- web/api/views.py
+++ web/api/views.py
@@ -1,23 +1,31 @@
 import logging
 log = logging.getLogger('web')
 
-from operator import contains
-from tastypie.authorization import Authorization
-from tastypie.authentication import Authentication
+from django.conf.urls import url
+
+from tastypie.authorization import Authorization, DjangoAuthorization
+from tastypie.authentication import Authentication, BasicAuthentication
 from tastypie.resources import ModelResource
 from tastypie.validation import Validation
-from tastypie.exceptions import NotFound
+from tastypie.exceptions import NotFound, ImmediateHttpResponse
+from tastypie import http
 
 from person.models import Person
 
+class PersonAuthentication(BasicAuthentication):
+    pass
 
-
+class PersonAuthorization(DjangoAuthorization):
+    pass
 
 class PersonResource(ModelResource):
     class Meta:
+        list_allowed_methods = ['get']
+        detail_allowed_methods = ['get', 'post']
+
         queryset = Person.objects.all()
-        authentication = Authentication()
-        authorization = Authorization()
+        authentication = PersonAuthentication()
+        authorization = PersonAuthorization()
 
     def _check_field_list(self, bundle, required_fields):
         return all(
@@ -25,6 +33,22 @@ class PersonResource(ModelResource):
                     lambda x: x in bundle.data, required_fields
                 )
             )
+
+    def override_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/$" % self._meta.resource_name, self.wrap_view('obj_logged_user'), name="api_logged_user"),
+            ]
+
+    def obj_logged_user(self, request, api_name, resource_name):
+        if not request.user.is_authenticated():
+            raise ImmediateHttpResponse(response=http.HttpUnauthorized())
+
+        bundle = self.build_bundle(obj=request.user.get_profile(), request=request)
+        bundle = self.full_dehydrate(bundle)
+        bundle = self.alter_detail_data_to_serialize(request, bundle)
+        return self.create_response(request, bundle)
+
+
     def obj_create(self, bundle, request=None):
         # simple registration
         simple_fields = (
@@ -32,8 +56,11 @@ class PersonResource(ModelResource):
         )
 
         vk_fields = (
-            'email', 'access_token'
+            'email', 'accesstoken'
         )
+
+        # TODO: check if already registred
+        # TODO: correct validation processing
 
         # is simple registration
         if self._check_field_list(bundle, simple_fields):
