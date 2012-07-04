6722b8d7163244ad255d13426b8278ecde4eb4d9
Ryan Romanchuk <rromanchuk@gmail.com>
2012-07-04 13:34:10 +0400

    lets make russia social

---
+++ ios/social/social/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  social
+//
+//  Created by Ryan Romanchuk on 7/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ios/social/socialTests/socialTests.h
@@ -0,0 +1,13 @@
+//
+//  socialTests.h
+//  socialTests
+//
+//  Created by Ryan Romanchuk on 7/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface socialTests : SenTestCase
+
+@end
