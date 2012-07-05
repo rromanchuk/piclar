bfdeabca370183102f243305447c7fbb794f6a01
Lazarev Ivan <ivan.lazarev@ostrovok.ru>
2012-07-05 12:41:39 +0400

    poimerge experiments

--- web/poi/management/commands/generate_baseword_dict.py
+++ web/poi/management/commands/generate_baseword_dict.py
@@ -15,9 +15,28 @@ class Command(BaseCommand):
             title = re.sub(u'[^A-Z0-9А-Я]', ' ', item.title.upper(), re.U)
             for word in title.split(' '):
                 word = word.strip()
-                if not word:
+                if len(word) < 3:
                     continue
-                word = morph.normalize(word).pop()
+                word = morph.normalize(word)
+                if isinstance(word, set):
+                    word = word.pop()
                 if word not in base_words:
                     base_words[word] = 1
-            break
+                else:
+                    base_words[word] +=1
+
+        def _cmp(x,y):
+            if x[1] < y[1]:
+                return 1
+            return -1
+        top = sorted(base_words.items(), cmp=_cmp)[:100]
+        f = file('basewords.txt', 'wb')
+        for item in top:
+            gram_info = morph.get_graminfo(item[0])
+            if gram_info:
+                gram_info = gram_info[0]['class']
+            else:
+                gram_info = ''
+            #print u"%s %s %s" % (item[0], item[1], gram_info)
+            #print item[0]
+            f.write(item[0].encode('utf-8') + '\n')
--- web/poi/management/commands/poimerge.py
+++ web/poi/management/commands/poimerge.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from django.core.management.base import BaseCommand, CommandError
 
 from poi.provider.altergeo.models import AltergeoPlace
@@ -5,22 +6,57 @@ from poi.provider.foursquare.models import FoursquarePlace
 
 from django.contrib.gis.geos import *
 from django.contrib.gis.measure import D
+from django.conf import settings
+from pymorphy import get_morph
 
+
+import re
 import logging
 import difflib
 
 log = logging.getLogger('web.poi.merger')
 
 class Command(BaseCommand):
-    def _normalize(self, text):
-        return
+
+    def __init__(self, *args, **kwargs):
+        self.base_words = set(file('basewords.txt', 'r'))
+        self.morph = get_morph(settings.DICTIONARY_PATH)
+
+        super(Command, self).__init__(*args, **kwargs)
+
+    def _normalize(self, text, to_cls):
+        result = []
+        text = re.sub(u'[^A-Z0-9А-Я]', ' ', text.upper(), re.U)
+        for word in text.split(' '):
+            word = word.strip()
+            if len(word) < 3:
+                continue
+            word = morph.normalize(word)
+            if isinstance(word, set):
+                word = word.pop()
+            result.append(word)
+        if not to_cls:
+            return ' '.join(result)
+        else:
+            return to_cls(result)
 
     def handle(self, *args, **options):
+
         # experiment with altergeo to fsq matching
         for item in FoursquarePlace.objects.all():
             to_compare = []
             for near_place in AltergeoPlace.objects.filter(position__distance_lte=(item.position, D(m=300))):
-                ratio = difflib.SequenceMatcher(None, near_place.title, item.title).ratio()
+                title1 = self._normalize(near_place.title, list)
+                title2 = self._normalize(item.title, list)
+
+                # remove base words
+                t1_set = set(title1)
+                t2_set = set(title2)
+                res_set = self.base_words.intersection(t1_set).intersection(t2_set)
+                if res_set:
+                    print res_set
+
+                ratio = difflib.SequenceMatcher(None, title1, title2).ratio()
                 to_compare.append({ 'a' : near_place, 'b' : item, 'ratio' : ratio})
 
             if len(to_compare) == 0:
