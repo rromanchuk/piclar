5fc081829489678097d103b929abf751f0a40a37
Lazarev Ivan <ivan.lazarev@ostrovok.ru>
2012-07-05 14:34:17 +0400

    rename poi to place in urls, fix place/search resutls

--- web/api/urls.py
+++ web/api/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
 from tastypie.api import Api
-from views import PersonResource, PoiResource
+from views import PersonResource, PlaceResource
 
 v1_api = Api(api_name='v1')
 v1_api.register(PersonResource())
-v1_api.register(PoiResource())
+v1_api.register(PlaceResource())
 
 urlpatterns = patterns('',
        url(r'^',include(v1_api.urls)),
--- web/poi/provider/foursquare/models.py
+++ web/poi/provider/foursquare/models.py
@@ -5,14 +5,13 @@ from django.contrib.gis.db import models
 class FoursquarePlace(models.Model):
     external_id = models.CharField(max_length=255, unique=True)
     external_data = models.TextField()
+    
 
     title = models.CharField(blank=False, null=False, max_length=255, verbose_name=u"Название места")
     description = models.TextField(blank=True, null=True, verbose_name=u"Описание места")
 
     phone = models.CharField(blank=True, null=True, max_length=255, verbose_name=u"Телефон")
-
     position = models.PointField(null=False, blank=False, verbose_name=u"Координаты места")
-
     type = models.CharField(blank=False, null=False, max_length=255, verbose_name=u"Тип места")
 
     city = models.CharField(max_length=255, null=True)
