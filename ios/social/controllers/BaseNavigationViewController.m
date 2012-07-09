ba7e3b4cd5120678a0cdcb4d0a83a6068d124748
Ryan Romanchuk <rromanchuk@gmail.com>
2012-07-09 10:12:18 +0400

    build out a little more of the interface

--- ios/social/controllers/BaseNavigationViewController.m
+++ ios/social/controllers/BaseNavigationViewController.m
@@ -1,18 +1,13 @@
-//
-//  BaseViewController.m
-//  explorer
-//
-//  Created by Ryan Romanchuk on 7/6/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
 
 #import "BaseNavigationViewController.h"
+#import "UIBarButtonItem+Borderless.h"
 
 @interface BaseNavigationViewController ()
 
 @end
 
 @implementation BaseNavigationViewController
+
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -30,6 +25,13 @@
         [self.navigationBar setBackgroundImage:[UIImage imageNamed:@"navbar.png"]
                                  forBarMetrics:UIBarMetricsDefault];
     }
+    self.navigationBar.titleTextAttributes = [NSDictionary dictionaryWithObjectsAndKeys:[UIFont fontWithName:@"HelveticaNeue" size:20.0], UITextAttributeFont, RGBACOLOR(204.0, 204.0, 204.0, 1.0), UITextAttributeTextColor, nil];
+    UIImage *gearImage = [UIImage imageNamed:@"checkin.png"];
+    UIImage *avatarImage = [UIImage imageNamed:@"profile.png"];
+    UIBarButtonItem *checkinButton = [UIBarButtonItem barItemWithImage:gearImage target:self action:@selector(didCheckIn:)];
+    UIBarButtonItem *avatarButton = [UIBarButtonItem barItemWithImage:avatarImage target:self action:@selector(didSelectSettings:)];
+    self.navigationBar.topItem.rightBarButtonItem = checkinButton;
+    self.navigationBar.topItem.leftBarButtonItem = avatarButton;
 	// Do any additional setup after loading the view.
 }
 
@@ -42,6 +44,14 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (IBAction)didCheckIn:(id)sender {
+    NSLog(@"did checkin");
+    
+}
+- (IBAction)didSelectSettings:(id)sender {
+    NSLog(@"did select settings");
 }
 
 @end
