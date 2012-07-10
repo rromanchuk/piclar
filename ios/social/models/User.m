ed8a3d7f72fb6cb20d2c37c5c4b6fa3639be70c1
Ryan Romanchuk <rromanchuk@gmail.com>
2012-07-10 17:31:51 +0400

    add simple mapper to user

--- ios/social/models/User.m
+++ ios/social/models/User.m
@@ -3,7 +3,7 @@
 #import "RestClient.h"
 
 static User *_currentUser = nil;
-static NSString *RESOURCE = @"person";
+static NSString *RESOURCE = @"api/v1/person/?format=json";
 @implementation User
 
 @synthesize first_name; 
@@ -13,6 +13,13 @@ static NSString *RESOURCE = @"person";
 @synthesize identifier;
 @synthesize token;
 
++ (NSDictionary *)mapping {
+    return [NSDictionary dictionaryWithObjectsAndKeys:
+    @"first_name", @"first_name",
+    @"last_name", @"last_name",
+    nil];
+}
+
 + (void)create:(NSMutableDictionary *)parameters
         onLoad:(void (^)(id object))onLoad
        onError:(void (^)(NSString *error))onError {
@@ -21,15 +28,24 @@ static NSString *RESOURCE = @"person";
     
     NSMutableURLRequest *request = [restClient requestWithMethod:@"POST" path:RESOURCE parameters:parameters];
     NSLog(@"Request is %@", request);
+    
     AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request 
                                                                                         success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+                                                                                            [[UIApplication sharedApplication] hideNetworkActivityIndicator];
                                                                                             NSLog(@"Name: %@ %@", [JSON valueForKeyPath:@"first_name"], [JSON valueForKeyPath:@"last_name"]);
+                                                                                            User *user = [User objectFromJSONObject:JSON mapping:[User mapping]];
+                                                                                            if (onLoad)
+                                                                                                onLoad(user);
                                                                                         } 
                                                                                         failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
+                                                                                            [[UIApplication sharedApplication] hideNetworkActivityIndicator];
+                                                                                            NSString *description = [[response allHeaderFields] objectForKey:@"X-Error"];
                                                                                             NSLog(@"There was a problem with the request");
                                                                                             NSLog(@"%@", error);
+                                                                                            if (onError)
+                                                                                                onError(description);
                                                                                         }];
-    
+    [[UIApplication sharedApplication] showNetworkActivityIndicator];
     [operation start];
 }
 
