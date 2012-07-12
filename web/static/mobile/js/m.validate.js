2b9be5ec0f2d0e33d6cf7983a00927a545e0de29
Max Degterev <me@maxdegterev.name>
2012-07-12 19:20:24 +0400

    frontend stuff

---
+++ web/static/mobile/js/m.validate.js
@@ -0,0 +1,207 @@
+/* Author:
+    Max Degterev @suprMax
+
+    Dependencies:
+        static/mobile/js/templates/s.validate.error.jst
+        templates-m/snippets/formerrors.html
+*/
+
+;(function($) {
+    var FValidate = function(settings) {
+        this.options = $.extend({
+            form: 'form',
+            template: MEDIA.templates['mobile/js/templates/s.validate.error.jst'].render,
+            validations: {},
+            report: false
+        }, settings);
+
+        this.els = {};
+        this.errors = [];
+
+        this.init();
+        this.logic();
+    };
+
+    FValidate.prototype.init = function() {
+        this.tmpl = this.options.template || false;
+        (typeof this.options.debug === 'function') && (this.debug = this.options.debug);
+        this.validation.external = this.options.validations;
+
+        this.els.form = $(this.options.form);
+        this.els.inputs = this.els.form.find('input[data-validate], textarea[data-validate]');
+        this.els.errors = this.els.form.find('.m-validate-errors');
+
+        this.load();
+    };
+
+    FValidate.prototype.load = function() {
+        var that = this;
+
+        var loadPattern = function(elem, index) {
+            var pattern = elem.getAttribute('data-pattern') || elem.getAttribute('pattern');
+
+            if (pattern) {
+                that.validation.patterns[elem.name] = new RegExp('^' + pattern + '$');
+            }
+        };
+
+        // Load patterns from HTML
+        this.els.inputs.filter('[data-filter*="pattern"]').forEach(loadPattern);
+    };
+
+    FValidate.prototype.logic = function() {
+        var that = this;
+
+        var handleSubmit = function(e) {
+            if (that.validateForm()) {
+                that.els.form.trigger('valid', e);
+                that.options.onFormValid && that.options.onFormValid.call(that, e);
+            }
+            else {
+                e.preventDefault();
+                that.els.form.trigger('invalid', e);
+                that.options.onFormInvalid && that.options.onFormInvalid.call(that, e);
+            }
+            that.handleErrors();
+        };
+
+        var handleInputFocus = function() {
+            $(this).removeClass('error');
+        };
+
+        var handleErrorClick = function(e) {
+            var relation = this.getAttribute('data-for');
+
+            if (relation && relation !== '__none__') {
+                this.parentNode.removeChild(this);
+
+                var input = that.els.inputs.filter('[name="' + relation + '"]');
+
+                // to avoid bug when sometimes keyboard doesn't show up
+                $.os.ios && that.els.inputs.blur();
+
+                input.length && input[0].focus();
+            }
+        };
+
+        this.els.form.attr('novalidate', 'novalidate');
+
+        this.els.form.on('submit', handleSubmit);
+        this.els.inputs.on('focus', handleInputFocus);
+        this.els.errors.on('click', '.m-validate-error', handleErrorClick);
+    };
+
+    FValidate.prototype.handleErrors = function() {
+        if (this.errors.length) {
+            this.els.errors.html(this.errors.join(''));
+            this.els.form.addClass('has_errors');
+        }
+        else {
+            this.els.errors.html('');
+            this.els.form.removeClass('has_errors');
+        }
+    };
+
+    FValidate.prototype.validateForm = function() {
+        var i, l, m, n,
+            input, el,
+            filters,
+            name,
+            failed;
+
+        this.errors = [];
+
+        for (i = 0, l = this.els.inputs.length; i < l; i++) {
+            input = this.els.inputs[i];
+            el = $(input);
+            name = input.name;
+            filters = el.data('filter') ? el.data('filter').split(' ') : [];
+            failed = [];
+
+            if (!el.attr('disabled')) {
+                el.attr('required') && filters.push('required');
+
+                for (m = 0, n = filters.length; m < n; m++) {
+                    this.validation.rules[filters[m]].call(input, this) || (failed.push(name + ':' + filters[m]));
+                }
+
+                if (typeof this.validation.external[name] === 'function') {
+                    this.validation.external[name].call(input, this) || (failed.push(name + ':external'));
+                }
+            }
+
+            if (!failed.length) {
+                el.removeClass('error');
+            }
+            else {
+                this.debug('[mod_validate]: "' + input.value + '" failed validation: ' + failed.join(', '));
+                el.addClass('error');
+                this.errors.push(this.tmpl ? this.tmpl({ name: name, error: el.data('error') }) : el.data('error'));
+                this.options.report && this.options.report.call(input, failed);
+            }
+        }
+
+        return !this.errors.length;
+    };
+
+    FValidate.prototype.validation = {
+        patterns: {},
+        external: {},
+        rules: {
+            required: function() {
+                if (this.type === 'radio' || this.type === 'checkbox') {
+                    return this.checked;
+                }
+                else {
+                    return !!this.value.trim().length;
+                }
+            },
+            email: function() {
+                return !!this.value.trim().length && /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(this.value);
+            },
+            number: function() {
+                var min = +this.getAttribute('min') || -Infinity,
+                    max = +this.getAttribute('max') || +Infinity,
+                    val = +this.value.trim();
+
+                return !isNaN(val) && val >= min && val <= max;
+            },
+            pattern: function(that) {
+                return that.validation.patterns[this.name].test(this.value);
+            }
+        }
+    };
+
+    FValidate.prototype.debug = function(msg) {
+    };
+
+    $.fn.mod_validate = function(settings) {
+        var options = $.extend({
+            validations: {}
+        }, settings);
+        
+        var report = function(failed) {
+            $.pub('validate_error', {
+                name: this.name,
+                value: this.value,
+                message: this.getAttribute('data-error'),
+                failed: failed
+            });
+        };
+
+        this.forEach(function(elem, index) {
+            var id = 'validate_' + (Math.random() * 99999 | 0);
+
+            elem.setAttribute('data-moduleid', id);
+            S.modules[id] = new FValidate({
+                form: elem,
+                debug: S.log,
+                validations: options.validations,
+                report: report
+            });
+
+            $.pub('validate_ready', id);
+            return S.modules[id];
+        });
+    };
+})(Zepto);
---
+++ web/static/mobile/js/s.overlay.js
@@ -0,0 +1,277 @@
+;S.loading = function () {
+    var layer = $('#l-loading'),
+        msg = layer.children('.l-loading-message'),
+        
+        loadingTxt = msg.data('loading'),
+        errorTxt = msg.data('error'),
+        successTxt = msg.data('success');
+
+    return {
+        start: function() {
+            msg.html(loadingTxt);
+            layer.css({ display: 'block' })
+                 .animate({ opacity: 1 }, 200, 'ease-in', function() {
+                     $.pub('loading_shown');
+                 });
+        },
+        stop: function() {
+            layer.animate({ opacity: 0 }, 200, 'ease-in', function() {
+                layer.css({ display: 'none' });
+                $.pub('loading_hidden');
+            });
+        },
+        error: function() {
+            msg.html(errorTxt);
+        },
+        success: function() {
+            msg.html(successTxt);
+        },
+        message: msg
+    };
+}();
+
+S.overlay = function() {
+    var layer = $('#l-overlay'),
+        close = $('#l-overlay-close'),
+        
+        inputs = layer.find('input, select, textarea'),
+
+        transform = S.browser.isOpera ? false : S.utils.supports('transform'),
+        settings,
+        isAnimated = false,
+        isVisible = false,
+        isFromOnStateChange = false,
+        hasHistory = 'pushState' in window.history && !window.location.hash,
+        pagePos = 0;
+        
+    if (!layer.length) {
+        return {
+            layer: null
+        };
+    }
+    
+    var handleKeyboard = function() {
+        var factor = function(e, i) {
+            e.blur();
+        };
+        inputs.forEach(factor);
+    };
+
+    var animateOverlayShow = function() {
+        if (transform) {
+            return function() {
+                var props = {};
+                isAnimated = true;
+
+                // TODO: make this shorter
+                switch (settings.slide) {
+                    case 'up':
+                        props[transform] = S.utils.translate(0, '-100%');
+                        layer.css({
+                                left: 0,
+                                top: '100%'
+                            });
+                        break;
+
+                    case 'right':
+                        props[transform] = S.utils.translate('100%', 0);
+                        layer.css({
+                                left: '-100%',
+                                top: 0
+                            });
+                        break;
+
+                    case 'down':
+                        props[transform] = S.utils.translate(0, '100%');
+                        layer.css({
+                                left: 0,
+                                top: '-100%'
+                            });
+                        break;
+
+                    case 'left':
+                        props[transform] = S.utils.translate('-100%', 0);
+                        layer.css({
+                                left: '100%',
+                                top: 0
+                            });
+                        break;
+                }
+                layer.animate(props, settings.duration, 'ease', animationShowEnd);
+            };
+        }
+        else {
+            return function() {
+                isAnimated = true;
+                switch (settings.slide) {
+                    case 'up':
+                        layer.css({
+                                left: 0,
+                                top: '100%'
+                            })
+                            .animate({ top: 0 }, settings.duration, 'ease', animationShowEnd);
+                        break;
+
+                    case 'right':
+                        layer.css({
+                                left: '-100%',
+                                top: 0
+                            })
+                            .animate({ left: 0 }, settings.duration, 'ease', animationShowEnd);
+                        break;
+
+                    case 'down':
+                        layer.css({
+                                left: 0,
+                                top: '-100%'
+                            })
+                            .animate({ top: 0 }, settings.duration, 'ease', animationShowEnd);
+                        break;
+
+                    case 'left':
+                        layer.css({
+                                left: '100%',
+                                top: 0
+                            }).animate({ left: 0 }, settings.duration, 'ease', animationShowEnd);
+                        break;
+                }
+            };
+        }
+    }();
+    var animateOverlayHide = function() {
+        if (transform) {
+            return function() {
+                var props = {};
+                isAnimated = true;
+                
+                props[transform] = S.utils.translate(0, 0);
+                layer.animate(props, settings.duration, 'ease', animationHideEnd);
+            };
+        }
+        else {
+            return function() {
+                isAnimated = true;
+                switch (settings.slide) {
+                    case 'up':
+                        layer.animate({ top: '100%' }, settings.duration, 'ease', animationHideEnd);
+                        break;
+
+                    case 'right':
+                        layer.animate({ left: '-100%' }, settings.duration, 'ease', animationHideEnd);
+                        break;
+
+                    case 'down':
+                        layer.animate({ top: '-100%' }, settings.duration, 'ease', animationHideEnd);
+                        break;
+
+                    case 'left':
+                        layer.animate({ left: '100%' }, settings.duration, 'ease', animationHideEnd);
+                        break;
+                }
+            };
+        }
+    }();
+    
+    var animationShowEnd = function() {
+        isAnimated = false;
+        isVisible = true;
+
+        settings.className.length && S.DOM.html.addClass(settings.className);
+        $.pub('overlay_show_end');
+    };
+    var animationHideEnd = function() {
+        isAnimated = false;
+        resetOverlay();
+
+        $.pub('overlay_hide_end');
+        isVisible = false;
+
+    };
+    var resetOverlay = function() {
+        close.removeClass('changed');
+        layer.css({
+            left: '-100%',
+            top: '-100%'
+        });
+    };
+    
+    var changeLabel = function() {
+        close.addClass('changed');
+    };
+    
+    var show = function(options) {
+        if (isAnimated) {
+            S.log('[S.overlay.show]: Already animating layer!');
+            return;
+        }
+
+        settings = $.extend({
+            slide: 'left',
+            duration: 200,
+            closable: true,
+            scrollable: true,
+            hideContent: true,
+            
+            className: ''
+        }, options);
+        
+        if (settings.closable) {
+            settings.className += ' has_overlay';
+        }
+        
+        if (settings.hideContent) {
+            settings.className += ' hide_main_cont';
+        }
+        
+        settings.scrollable || S.DOM.doc.on('touchmove', S.e);
+
+        pagePos = (document.body.scrollTop === 0 || document.body.scrollTop === 1) ? 0 : document.body.scrollTop;
+        pagePos && S.utils.scroll();
+        
+        $.pub('overlay_show_start');
+        animateOverlayShow();
+        
+        // If the browser supports push states, add overlay state which will respond to back button
+        // presses and properly close the overlay modal
+        hasHistory && window.history.pushState({overlay: 'true'}, document.title, "#overlay");
+
+    };
+    
+    var hide = function(e) {
+        e && S.e(e);
+        settings.className.length && S.DOM.html.removeClass(settings.className);
+        //contentWrap.prepend(contentBlock);
+        
+        // Hides keyboard and other inputs
+        handleKeyboard();
+        
+        pagePos && S.utils.scroll(pagePos);
+        settings.scrollable || S.DOM.doc.off('touchmove', S.e);
+    
+        $.pub('overlay_hide_start');
+        animateOverlayHide();
+        // Since we dont support showing the overlay on backbutton press, let's pop the history stack
+        // to not mess up the user's back button 
+        if ((hasHistory && location.hash) || (hasHistory && !isFromOnStateChange)) {
+            window.history.go(-1);
+            isFromOnStateChange = false;
+        }
+    };
+    
+    var handlePopState = function(e) {
+        isFromOnStateChange = true; 
+        isVisible && S.overlay.hide();
+    };
+    
+    transform && layer.css(transform, S.utils.translate(0, 0));
+    close.onpress(hide);
+    
+    $.pub('overlay_ready');
+    hasHistory && S.DOM.win.on('popstate', handlePopState);
+    return {
+        layer: layer,
+        show: show,
+        hide: hide,
+        changed: changeLabel
+    };
+}();
