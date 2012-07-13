c49be226c5fb32a3e2e7380dd78b088ce7240a52
Ryan Romanchuk <rromanchuk@gmail.com>
2012-07-13 17:33:43 +0400

    start building the PlaceShow slide 02C

---
+++ ios/social/views/PlaceMapDetailCell.m
@@ -0,0 +1,29 @@
+//
+//  PlaceMapDetailCell.m
+//  explorer
+//
+//  Created by Ryan Romanchuk on 7/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PlaceMapDetailCell.h"
+
+@implementation PlaceMapDetailCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
