1a89d749bb1771e6427f347a5836007b471182af
Max Degterev <max@ostrovok.ru>
2012-07-17 18:56:38 +0400

    registration form

--- web/templates/blocks/page-users_registration/p-users_registration.js
+++ web/templates/blocks/page-users_registration/p-users_registration.js
@@ -1,8 +1,37 @@
 (function($){
     var form = S.DOM.content.find('.p-r-form'),
-        button = form.find('button');
+        button = form.find('button'),
 
-    form.m_validate();
+        inputs = form.find('input'),
 
-    button.removeAttr('disabled');
+        pw1 = inputs.filter('.p-r-f-password'),
+        pw2 = inputs.filter('.p-r-f-password2'),
+
+        inactive = inputs.length;
+
+    var activateInput = function() {
+        if (this.value.length) {
+            if (this.active) {
+                return;
+            }
+            this.active = true;
+            inactive--;
+
+            if (inactive <= 0) {
+                button.removeAttr('disabled');
+            }
+        }
+    };
+
+    var checkPasswords = function() {
+        return pw1.val() === pw2.val();
+    };
+
+    form.m_validate({
+        validations: {
+            password2: checkPasswords
+        }
+    });
+
+    inputs.on('keyup', activateInput);
 })(jQuery);
