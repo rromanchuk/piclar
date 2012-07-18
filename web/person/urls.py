cbd7dc013247fa7d6462592f01dccb8e7d261f28
Max Degterev <max@ostrovok.ru>
2012-07-18 17:32:30 +0400

    fixs

--- web/person/urls.py
+++ web/person/urls.py
@@ -4,7 +4,7 @@ urlpatterns = patterns('',
     url(r'registration/$', 'person.views.registration', name='person-registration'),
     url(r'oauth/$', 'person.views.oauth', name='person-oauth'),
 
-    url(r'login/$', 'django.contrib.auth.views.login', { 'template_name' : 'blocks/page-users_login/p-users_login.html'  }, name='person-login'),
+    url(r'login/$', 'django.contrib.auth.views.login', { 'template_name' : 'blocks/page-users-login/p-users-login.html'  }, name='person-login'),
     url(r'logout/$', 'django.contrib.auth.views.logout', name='person-logout'),
     url(r'preregistration/$', 'person.views.preregistration', name='person-preregistration'),
 )
