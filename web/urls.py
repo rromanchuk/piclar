854c6ae3b6053728fa26ed9301d25ec6a3d09a97
Max Degterev <max@ostrovok.ru>
2012-07-19 21:51:38 +0400

    basic feed

--- web/urls.py
+++ web/urls.py
@@ -8,6 +8,9 @@ urlpatterns = patterns('',
     url(r'^m/', include('mobile.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'poi.views.index', name='page-index'),
+
+    url(r'feed/$', 'django.shortcuts.render', {'template_name': 'blocks/page-feed/p-feed.html'}, name='feed'),
+
     url(r'^test/$', 'poi.views.test', name='page-test'),
     url(r'^users/', include('person.urls')),
 )
