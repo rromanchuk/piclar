29b5963ae32e5600aff3d3de23e0619731ed4cd1
Max Degterev <me@maxdegterev.name>
2012-07-30 20:14:32 +0400

    photogallery

--- web/templates/blocks/page-place/p-place.js
+++ web/templates/blocks/page-place/p-place.js
@@ -1,3 +1,7 @@
+// @require 'blocks/block-photogallery/b-photogallery.js'
+// @require 'blocks/block-photogallery-small/b-photogallery-small.js'
+
 (function($){
+    var pg = new S.blockPhotoGallery().init();
 
 })(jQuery);
