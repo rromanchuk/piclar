7cc4a28fa08efa9d2140c535846bc6fc86dba5f9
Max Degterev <me@maxdegterev.name>
2012-07-30 22:01:29 +0400

    bullox

--- web/templates/blocks/page-place/p-place.js
+++ web/templates/blocks/page-place/p-place.js
@@ -15,7 +15,7 @@
             center: new google.maps.LatLng(-34.397, 150.644),
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-        var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+        var map = new google.maps.Map($('.p-p-map')[0], mapOptions);
     }
 
     loadMap();
