6dc4f3db27aaa3af6ac4dc7af36eca99d66e8211
Max Degterev <me@maxdegterev.name>
2012-07-31 16:29:07 +0400

    map w market

--- web/templates/blocks/page-place/p-place.js
+++ web/templates/blocks/page-place/p-place.js
@@ -2,7 +2,10 @@
 // @require 'blocks/block-photogallery-small/b-photogallery-small.js'
 
 (function($){
-    var pg = new S.blockPhotoGallery().init(),
+    var page = S.DOM.content,
+        map = page.find('.p-p-map'),
+
+        pg = new S.blockPhotoGallery().init(),
         pgs = new S.blockPhotoGallerySmall().init();
 
     var handleThumbClick = function(e, data) {
@@ -10,12 +13,17 @@
     };
 
     function loadMap() {
-        var mapOptions = {
-            zoom: 8,
-            center: new google.maps.LatLng(-34.397, 150.644),
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map($('.p-p-map')[0], mapOptions);
+        var gMapOptions = {
+                zoom: S.data.place.zoom,
+                center: new google.maps.LatLng(S.data.place.position.latitude, S.data.place.position.longitude),
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            },
+            gMapObj = new google.maps.Map(map[0], gMapOptions),
+            gMarkerObj = new google.maps.Marker({
+                //icon: 'beachflag.png',
+                position: new google.maps.LatLng(S.data.place.position.latitude, S.data.place.position.longitude),
+                map: gMapObj
+            });
     }
 
     loadMap();
