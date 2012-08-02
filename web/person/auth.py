fdac1e87c685f88eafc3530437647976411f8986
Lazarev Ivan <ivan.lazarev@ostrovok.ru>
2012-08-02 15:44:49 +0400

    add http protocol to image urls

--- web/person/auth.py
+++ web/person/auth.py
@@ -1,6 +1,22 @@
 from django.contrib.auth.decorators import login_required as django_login_required
+from django.contrib.auth.decorators import user_passes_test
 
-def login_required(f):
-        def wrap(request, *args, **kwargs):
-            pass
-        return django_login_required(f)
+
+def login_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
+    """
+    Decorator for views that checks that the user is logged in, redirecting
+    to the log-in page if necessary.
+    """
+    login_decorator = user_passes_test(
+        lambda u: u.is_authenticated(),
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    active_decodator = user_passes_test(
+        lambda u: u.get_profile().status == ,
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
