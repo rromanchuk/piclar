310039f0873aa10aacbc5f75a7b5ec4db2e73523
Max Degterev <me@maxdegterev.name>
2012-08-03 19:39:36 +0400

    handleSubscribe

--- web/templates/blocks/page-users-profile/p-users-profile.js
+++ web/templates/blocks/page-users-profile/p-users-profile.js
@@ -1,7 +1,37 @@
 // @require 'blocks/block-subscriptions/b-subscriptions.js'
 
 (function($){
-    window.x = new S.blockSubscriptions({
+    var blockSubs = $('.p-u-p-subscribe');
+
+    if (blockSubs.length) {
+        var subscribe = blockSubs.find('.p-u-p-link-subscribe'),
+            unsubscribe = blockSubs.find('.p-u-p-link-unsubscribe');
+
+        var handleAjaxError = function() {
+            S.notifications.show({
+                type: 'error',
+                text: 'Произошла ошибка при обращении к серверу. Пожалуйста, попробуйте еще раз.'
+            });
+        };
+
+        var handleRequest = function() {
+            var el = $(this),
+                subscribe = el.hasClass('p-u-p-link-subscribe');
+
+            $.ajax({
+                url: S.urls.subscriptions,
+                data: { userid: item.data('userid') },
+                type: subscribe ? 'PUT' : 'DELETE',
+                dataType: 'json',
+                error: handleAjaxError
+            });
+        };
+
+        subscribe.on('click', handleRequest);
+        unsubscribe.on('click', handleRequest);
+    }
+
+    new S.blockSubscriptions({
         data: S.data.subscriptions
     }).init();
 })(jQuery);
