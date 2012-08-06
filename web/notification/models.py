066b4ac619927cd8ef78b3ebccd582ebe41864c0
Lazarev Ivan <ivan.lazarev@ostrovok.ru>
2012-08-06 20:10:06 +0400

    notifications proto

---
+++ web/notification/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+'''
+class Notification(models.Model):
+    NOTIFICATION_TYPE_NEW_COMMENT = 1
+    NOTIFICATION_TYPE_NEW_FRIEND = 2
+
+    sender = models.ForeignKey('Perosn', null=True)
+    receiver = models.ForeignKey('Person')
+'''
+
+
+
