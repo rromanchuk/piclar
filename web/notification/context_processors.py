f9e99f3e438bae5e2339396d725168a58de91472
Lazarev Ivan <ivan.lazarev@ostrovok.ru>
2012-08-07 21:43:11 +0400

    fix broken index

--- web/notification/context_processors.py
+++ web/notification/context_processors.py
@@ -3,7 +3,7 @@ from notification.models import Notification
 
 def notifications(request):
     if not request.user or not request.user.is_authenticated() or request.user.get_profile().status != Person.PERSON_STATUS_ACTIVE:
-        return
+        return {}
 
     person = request.user.get_profile()
     notification_list =  Notification.objects.get_person_notifications_popup(person)
