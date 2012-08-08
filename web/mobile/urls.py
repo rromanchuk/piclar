0f25fd6b726a301bb3d32e220cb553fed6dc788f
Max Degterev <me@maxdegterev.name>
2012-08-08 18:01:49 +0400

    comments p

--- web/mobile/urls.py
+++ web/mobile/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-   url(r'^$', 'mobile.views.index', name='mobile_index'),
-   url(r'users/login/$', 'mobile.views.login', name='mobile_login'),
-   url(r'users/oauth/$', 'mobile.views.oauth', name='mobile_oauth'),
+    url(r'^$', 'mobile.views.index', name='mobile_index'),
+    url(r'users/login/$', 'mobile.views.login', name='mobile_login'),
+    url(r'users/oauth/$', 'mobile.views.oauth', name='mobile_oauth'),
 
+
+    url(r'^comments/$', 'django.shortcuts.render', dict(template_name='pages/m_comments.html')),
 )
