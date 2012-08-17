1d8acfc072b274c72cedd9d977fd5d59b66a57ee
Lazarev Ivan <ivan.lazarev@ostrovok.ru>
2012-08-17 14:47:35 +0400

    migration + inctive_url param for login_required decorator

--- web/person/auth.py
+++ web/person/auth.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import user_passes_test
 from django.conf import settings
 from person.models import Person
 
-def login_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None, skip_test_active=False):
+def login_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None, inactive_url=None, skip_test_active=False):
     """
     Decorator for views that checks that the user is logged in, redirecting
     to the log-in page if necessary.
@@ -14,9 +14,11 @@ def login_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login
         login_url=login_url,
         redirect_field_name=redirect_field_name
     )
+    if not inactive_url:
+        inactive_url = settings.INACTIVE_USER_REDIRECT_URL
     active_decorator = user_passes_test(
         lambda u: u.is_authenticated() and  u.get_profile().status == Person.PERSON_STATUS_ACTIVE,
-        login_url=settings.INACTIVE_USER_REDIRECT_URL,
+        login_url=inactive_url,
         redirect_field_name=redirect_field_name
     )
 
