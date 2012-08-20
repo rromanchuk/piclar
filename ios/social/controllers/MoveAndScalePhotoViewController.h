984779a994ce471b4cab5664bbdce2a344d75ba0
Ryan Romanchuk <rromanchuk@gmail.com>
2012-08-20 15:42:14 +0400

    new move and scale screen for photo libary photos

---
+++ ios/social/controllers/MoveAndScalePhotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoveAndScalePhotoViewController.h
+//  Ostronaut
+//
+//  Created by Ryan Romanchuk on 8/20/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoveAndScalePhotoViewController : UIViewController
+
+@end
