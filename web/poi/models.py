e3701832d369c7d94dc498d6d5bd7ac367823472
Lazarev Ivan <ivan.lazarev@ostrovok.ru>
2012-08-22 18:43:19 +0400

    show checkin photos if we have no moderated place photos

--- web/poi/models.py
+++ web/poi/models.py
@@ -115,7 +115,17 @@ class Place(models.Model):
         return Checkin.objects.filter(place=self).distinct('person').order_by('person', 'create_date')[:20]
 
     def get_photos_url(self):
-        return [photo.url for photo in self.placephoto_set.filter(moderated_status=PlacePhoto.MODERATED_GOOD)[:10]]
+        placephotos_qs = self.placephoto_set.filter(moderated_status=PlacePhoto.MODERATED_GOOD)
+        if placephotos_qs.count() > 0:
+            return [photo.url for photo in placephotos_qs[:10]]
+
+        urls = []
+        for checkin in Checkin.objects.filter(place=self).order_by('-create_date')[:10]:
+            photos = checkin.checkinphoto_set.all()
+            if photos.count() > 0:
+                urls.append(photos[0].url)
+        return urls
+
 
     def sync_gis_region(self):
         from gisclient import region_by_coords
@@ -260,7 +270,7 @@ class Checkin(models.Model):
             'rate': self.rate,
             'review' : self.review,
             'place_id': self.place.id,
-            'photos': [ { 'id': photo.id, 'title' : photo.title, 'url' : photo.photo.url.replace('orig', settings.CHECKIN_IMAGE_FORMAT_640) } for photo in self.checkinphoto_set.all() ]
+            'photos': [ { 'id': photo.id, 'title' : photo.title, 'url' : photo.url } for photo in self.checkinphoto_set.all() ]
         }
         return proto
 
@@ -276,3 +286,7 @@ class CheckinPhoto(models.Model):
         verbose_name=u"Фото пользователя"
     )
     provider = models.CharField(blank=True, null=True, max_length=255, verbose_name=u"Провайдер")
+
+    @property
+    def url(self):
+        return self.photo.url.replace('orig', settings.CHECKIN_IMAGE_FORMAT_640)
