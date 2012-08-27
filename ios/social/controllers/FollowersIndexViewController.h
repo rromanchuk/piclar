c87618279f5e7193112583a814f65de9851a049c
Ryan Romanchuk <rromanchuk@gmail.com>
2012-08-27 18:56:31 +0400

    fix some bugs with search, add search to followers screen

--- ios/social/controllers/FollowersIndexViewController.h
+++ ios/social/controllers/FollowersIndexViewController.h
@@ -8,7 +8,23 @@
 
 #import "CoreDataTableViewController.h"
 #import "User+Rest.h"
-@interface FollowersIndexViewController : CoreDataTableViewController
+@interface FollowersIndexViewController : UITableViewController <UISearchBarDelegate, NSFetchedResultsControllerDelegate, UISearchDisplayDelegate> {
+    // required ivars for this example
+    NSFetchedResultsController *fetchedResultsController_;
+    NSFetchedResultsController *searchFetchedResultsController_;
+    NSManagedObjectContext *managedObjectContext_;
+    
+    // The saved state of the search UI if a memory warning removed the view.
+    NSString        *savedSearchTerm_;
+    NSInteger       savedScopeButtonIndex_;
+    BOOL            searchWasActive_;
+}
+
 @property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 @property (strong, nonatomic) User *user;
+@property (strong, nonatomic) NSString *savedSearchTerm;
+@property NSInteger savedScopeButtonIndex;
+@property BOOL searchWasActive;
+@property (strong, nonatomic) IBOutlet UITableView *_tableView;
+
 @end
