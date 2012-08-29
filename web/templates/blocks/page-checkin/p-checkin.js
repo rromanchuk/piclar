aa6305c4842fa46c87c18ea5717f3a3f6be840b0
Max Degterev <me@maxdegterev.name>
2012-08-29 17:52:21 +0400

    checkin page improvements

--- web/templates/blocks/page-checkin/p-checkin.js
+++ web/templates/blocks/page-checkin/p-checkin.js
@@ -2,10 +2,32 @@
 
 (function($){
     var page = S.DOM.content,
-        content = page.find('.p-c-content');
+        content = page.find('.p-c-content'),
+
+        photo = content.find('.b-s-f-image'),
+
+        overlayPart = '.p-c-photo-view',
+        overlay = S.overlay.parts.filter(overlayPart);
+
+    var handlePhotoClick = function(e) {
+        S.e(e);
+
+        overlay.html('<img src="' + this.getAttribute('src') + '" />');
+        S.overlay.show({
+            block: overlayPart
+        });
+    };
+
+    var handleOverlayClick = function(e) {
+        S.e(e);
+        S.overlay.hide();
+    };
 
     new S.blockStoryFull({
         elem: content.find('.b-story-full')
     }).init();
 
+    photo.css({ cursor: 'pointer' });
+    photo.on('click', handlePhotoClick);
+    overlay.on('click', 'img', handleOverlayClick);
 })(jQuery);
