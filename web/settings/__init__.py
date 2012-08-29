0eb002484d7d2f0e799c7f0dbcd62535c0bd6e1c
Lazarev Ivan <ivan.lazarev@ostrovok.ru>
2012-08-29 13:21:33 +0400

    fix settings

--- web/settings/__init__.py
+++ web/settings/__init__.py
@@ -2,6 +2,15 @@ from base import *
 from logging_settings import *
 from media import *
 
+from base import DEBUG
+if DEBUG == False:
+    # disable logging to sentry on debug
+    LOGGING['root']  =  {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+        },
+
+
 try:
     from local_settings import *
 except ImportError:
