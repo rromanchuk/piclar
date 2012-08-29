0bc67b6c4efe58fa2e21db5b63ec800db4ff2965
Max Degterev <me@maxdegterev.name>
2012-08-29 16:08:03 +0400

    agreement page

--- web/urls.py
+++ web/urls.py
@@ -16,6 +16,7 @@ urlpatterns = patterns('',
     url(r'^feedback/', include('feedback.urls')),
 
     url(r'^about/$', 'django.shortcuts.render', dict(template_name='blocks/page-about/p-about.html'), name='page-about'),
+    url(r'^agreement/$', 'django.shortcuts.render', dict(template_name='blocks/page-agreement/p-agreement.html'), name='page-agreement'),
     url(r'^404/$', 'django.shortcuts.render', dict(template_name='blocks/page-error404/p-error404.html')),
     url(r'^500/$', 'django.shortcuts.render', dict(template_name='blocks/page-error500/p-error500.html')),
 
