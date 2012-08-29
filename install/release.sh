b0267ad6deb8ff960efb24a8bc31298603f342ec
Lazarev Ivan <ivan.lazarev@ostrovok.ru>
2012-08-29 15:41:50 +0400

    release script update

--- install/release.sh
+++ install/release.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 PROJPATH=/var/www/social
 pushd $PROJPATH
+
+echo "Starting release"
+
 git pull
-rm -rf /ios
+
+if [ $? -ne 0 ]; then
+    echo "Git update failed. Release stoped."
+    popd
+    exit 1
+fi
+
+rm -rf $PROJPATH/ios
+
 web/manage.py syncdb
 web/manage.py migrate
 web/manage.py generatemedia
 web/manage.py collectstatic --noinput
 web/manage.py generate_pages
 service uwsgi restart
+
+echo "Release done".
 popd
