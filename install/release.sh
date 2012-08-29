6ca2e1796e999adceb81a54c2e0ae86e0719af07
Lazarev Ivan <ivan.lazarev@ostrovok.ru>
2012-08-29 15:36:40 +0400

    fix release script

--- install/release.sh
+++ install/release.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-PATH=/var/www/social
-
-pushd $PATH
-
+PROJPATH=/var/www/social
+pushd $PROJPATH
 git pull
-
 rm -rf /ios
-
 web/manage.py syncdb
 web/manage.py migrate
 web/manage.py generatemedia
-web/manage.py collectstatic
+web/manage.py collectstatic --noinput
 web/manage.py generate_pages
-
-service uswgi restart
-
+service uwsgi restart
 popd
