3d4fe67466de4d0ddb1e1d10deb126b5975ef5d2
Lazarev Ivan <ivan.lazarev@ostrovok.ru>
2012-09-07 13:59:22 +0400

    finish resetpassword

--- web/person/urls.py
+++ web/person/urls.py
@@ -17,9 +17,9 @@ urlpatterns = patterns('',
     url(r'login/$', 'person.views.login', name='person-login'),
     url(r'logout/$', 'django.contrib.auth.views.logout', { 'next_page' : '/' } , name='person-logout'),
     url(r'preregistration/$', 'person.views.preregistration', name='person-preregistration'),
-    url(r'passwordreset/$', 'person.views.password_reset', name='person-resetpassword'),
-    url(r'passwordreset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', name='person-resetpassword-confirm'),
-    url(r'passwordreset/done/$', 'person.views.password_reset_done', name='password_reset_done')
+    url(r'passwordreset/$', 'person.views.password_reset', name='person-passwordreset'),
+    url(r'passwordreset/done/$', 'django.views.generic.simple.direct_to_template', {'template': 'blocks/page-users-resetpassword-done/p-users-resetpassword-done.html'}, name='person-passwordreset-done'),
+    url(r'passwordreset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', { 'template_name' :  'blocks/page-users-resetpassword-confirm/p-users-resetpassword-confirm.html', 'post_reset_redirect' : '/' }, name='person-passwordreset-confirm'),
 
 
 
---
+++ web/templates/blocks/page-users-resetpassword-confirm/p-users-resetpassword-confirm.js
@@ -0,0 +1,7 @@
+(function($){
+    var form = S.DOM.content.find('.p-u-r-c-form');
+
+    form.m_validate({
+        isDisabled: true
+    });
+})(jQuery);
