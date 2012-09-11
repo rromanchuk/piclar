c6c8f0a32124d49d22dfa795d399767d0b1157f9
Ryan Romanchuk <rromanchuk@gmail.com>
2012-09-11 20:11:25 +0400

    start adding support for notifications

---
+++ ios/social/controllers/PlaceCreateViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlaceCreateViewController.h
+//  Ostronaut
+//
+//  Created by Ryan Romanchuk on 9/11/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlaceCreateViewController : UITableViewController
+
+@end
---
+++ ios/social/models/RestNotification.h
@@ -0,0 +1,21 @@
+//
+//  RestNotification.h
+//  Ostronaut
+//
+//  Created by Ryan Romanchuk on 9/11/12.
+//
+//
+
+#import "RestObject.h"
+
+@interface RestNotification : RestObject
+@property (strong, atomic) NSString *type;
+@property (strong, atomic) NSDate *createdAt;
+@property NSInteger isRead;
+@property NSInteger notificationType;
+
++ (NSDictionary *)mapping;
++ (void)load:(void (^)(NSSet *notificationItems))onLoad
+     onError:(void (^)(NSString *error))onError;
+
+@end
