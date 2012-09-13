38fc2e9a9b97ae2a16e797ffa77b5f9c6ad9747c
Ryan Romanchuk <rromanchuk@gmail.com>
2012-09-13 20:42:57 +0400

    more work with user settings

--- ios/social/models/UserSettings+Rest.h
+++ ios/social/models/UserSettings+Rest.h
@@ -7,7 +7,17 @@
 //
 
 #import "UserSettings.h"
+#import "RestObject.h"
+#import "RestUserSettings.h"
+#import "User.h"
 
 @interface UserSettings (Rest)
 
++ (UserSettings *)userSettingsWithRestNotification:(RestUserSettings *)restUserSettings inManagedObjectContext:(NSManagedObjectContext *)context forUser:(User *)user;
+
+- (void)pushToServer:(void (^)(RestUserSettings *restUser))onLoad
+             onError:(void (^)(NSString *error))onError;
+
+- (void)setManagedObjectWithIntermediateObject:(RestObject *)intermediateObject;
+- (void)updateUserSettingsWithRestUserSettings:(RestUserSettings *)restUserSettings;
 @end
