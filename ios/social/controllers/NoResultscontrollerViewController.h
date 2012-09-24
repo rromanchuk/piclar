9983f821bc429a7bc49c603827bb08c797f3c61e
Ryan Romanchuk <rromanchuk@gmail.com>
2012-09-24 19:01:17 +0400

    wire up outlets

--- ios/social/controllers/NoResultscontrollerViewController.h
+++ ios/social/controllers/NoResultscontrollerViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface NoResultscontrollerViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *noResultsLabel;
+@property (weak, nonatomic) IBOutlet UIButton *noResultsCheckinButton;
 
 @end
--- ios/social/controllers/NoResultscontrollerViewController.m
+++ ios/social/controllers/NoResultscontrollerViewController.m
@@ -35,4 +35,9 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)viewDidUnload {
+    [self setNoResultsLabel:nil];
+    [self setNoResultsCheckinButton:nil];
+    [super viewDidUnload];
+}
 @end
