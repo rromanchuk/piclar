711d8b6bd808f8f2a494d02cc965f52d63fb8ebe
Max Degterev <me@maxdegterev.name>
2012-10-02 19:34:39 +0400

    sticky

--- web/templates/blocks/page-favorites/p-favorites.js
+++ web/templates/blocks/page-favorites/p-favorites.js
@@ -6,6 +6,15 @@
 
         feedList = page.find('.b-f-list'),
 
+        citiesBlock = page.find('.p-f-cities'),
+        mapsBlock = page.find('.b-favorites-map'),
+
+        citiesPos = citiesBlock.offset().top,
+        mapsPos = mapsBlock.offset().top - 40,
+
+        citiesFixed = false,
+        mapsFixed = false,
+
         feed = new S.blockFavorites({
             data: S.data.favorites
         }),
@@ -20,10 +29,33 @@
         feed.setActive(id);
     };
 
+    var handleWindowScroll = _.debounce(function(e) {
+        var pos = S.DOM.win.scrollTop();
+
+        if (pos > citiesPos && !citiesFixed) {
+            citiesBlock.addClass('fixed');
+            citiesFixed = true;
+        }
+        else if (pos <= citiesPos && citiesFixed) {
+            citiesBlock.removeClass('fixed');
+            citiesFixed = false;
+        }
+
+        if (pos > mapsPos && !mapsFixed) {
+            mapsBlock.addClass('fixed');
+            mapsFixed = true;
+        }
+        else if (pos <= mapsPos && mapsFixed) {
+            mapsBlock.removeClass('fixed');
+            mapsFixed = false;
+        }
+    }, 1000 / 60); // 60fps
+
     feed.init();
     map.init();
 
     feedList.on('mouseenter', '.b-f-place', updateCurrentPlace);
+    S.DOM.win.on('scroll', handleWindowScroll);
 
     window.map = map;
 })(jQuery);
