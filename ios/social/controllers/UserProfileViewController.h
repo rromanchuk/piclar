28da76e9f7e03c6841388257759f0c1498945aed
Ryan Romanchuk <rromanchuk@gmail.com>
2012-10-15 17:22:47 +0400

    new user profile page

---
+++ ios/social/controllers/UserProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserProfileViewController.h
+//  Ostronaut
+//
+//  Created by Ryan Romanchuk on 10/15/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserProfileViewController : UIViewController
+
+@end
---
+++ ios/social/controllers/UserProfileViewController.m
@@ -0,0 +1,38 @@
+//
+//  UserProfileViewController.m
+//  Ostronaut
+//
+//  Created by Ryan Romanchuk on 10/15/12.
+//
+//
+
+#import "UserProfileViewController.h"
+
+@interface UserProfileViewController ()
+
+@end
+
+@implementation UserProfileViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
