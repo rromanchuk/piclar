2774d44b7dc36f694b4032b309395224710db9ef
Lazarev Ivan <ivan.lazarev@ostrovok.ru>
2012-10-19 20:39:51 +0400

    fix feed prefetch

--- web/feed/models.py
+++ web/feed/models.py
@@ -49,6 +49,9 @@ class FeedItemManager(models.Manager):
         for pitem in qs:
             item_type = pitem.item.type
             field_value = pitem.item.data[item_type][field]
+            if field_value not in prefetched:
+                del pitem.item.data[item_type][field]
+                continue
             pitem.item.data[item_type][assign] = prefetched[field_value]
             del pitem.item.data[item_type][field]
 
