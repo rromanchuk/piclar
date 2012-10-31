494fea6ba5654c9f9d0c139efd1536a3e3dc60c2
Ryan Romanchuk <rromanchuk@gmail.com>
2012-10-31 22:14:50 +0400

    start working on new profile page

---
+++ ios/social/controllers/CoreDataCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  CoreDataCollectionViewController.h
+//  Ostronaut
+//
+//  Created by Ryan Romanchuk on 10/31/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoreDataCollectionViewController : UICollectionViewController
+
+@end
---
+++ ios/social/controllers/CoreDataCollectionViewController.m
@@ -0,0 +1,43 @@
+//
+//  CoreDataCollectionViewController.m
+//  Ostronaut
+//
+//  Created by Ryan Romanchuk on 10/31/12.
+//
+//
+
+#import "CoreDataCollectionViewController.h"
+
+@interface CoreDataCollectionViewController ()
+
+@end
+
+@implementation CoreDataCollectionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 1;
+}
+
+@end
