40dc4fc42a90f881c33758b1f5f7ca1126735afa
Ryan Romanchuk <ryan@ostrovok.ru>
2012-11-08 14:38:31 +0400

    add some more debugging, turn on fb, more desgin polish

--- ios/social/helpers/FacebookHelper.m
+++ ios/social/helpers/FacebookHelper.m
@@ -10,29 +10,18 @@
 #import "RestUser.h"
 @implementation FacebookHelper
 + (void)openSession {
-    NSArray *permissions = [NSArray arrayWithObjects:@"email", nil];
+    NSArray *permissions = [NSArray arrayWithObjects:@"email", @"publish_actions", nil];
     [FBSession openActiveSessionWithPermissions:permissions allowLoginUI:YES
                               completionHandler:^(FBSession *session,
                                                   FBSessionState status,
                                                   NSError *error) {
                                   
-                                  if([RestUser currentUserToken]) {
-                                      [RestUser updateToken:session.accessToken
-                                                     onLoad:^(RestUser *restUser) {
-                                                                                                                  
-                                                     } onError:^(NSString *error) {
-                                                         DLog(@"error %@", error);
-                                                         
-                                                     }];
-                                  } else {
-                                      DLog(@"no existing token");
-                                      [FacebookHelper sessionStateChanged:session state:status error:error];
-                                  }
-                                  
+                                  [FacebookHelper sessionStateChanged:session state:status error:error];
                                   
                               }];
     
 }
+
 
 + (void)sessionStateChanged:(FBSession *)session
                       state:(FBSessionState) state
@@ -41,14 +30,29 @@
     switch (state) {
         case FBSessionStateOpen: {
             
-            FBRequest *me = [FBRequest requestForMe];
-            [me startWithCompletionHandler: ^(FBRequestConnection *connection,
-                                              NSDictionary<FBGraphUser> *my,
-                                              NSError *error) {
-                DLog(@"got data from facebook %@", my);
+//            FBRequest *me = [FBRequest requestForMe];
+//            [me startWithCompletionHandler: ^(FBRequestConnection *connection,
+//                                              NSDictionary<FBGraphUser> *my,
+//                                              NSError *error) {
+//                DLog(@"got data from facebook %@", my);
+//                
+//                
+//            }];
+            
+            // Update server with new token
+            if([RestUser currentUserToken]) {
+                [RestUser updateToken:session.accessToken
+                               onLoad:^(RestUser *restUser) {
+                                   
+                               } onError:^(NSString *error) {
+                                   DLog(@"error %@", error);
+                                   
+                               }];
+            } else {
+                DLog(@"no existing token");
                 
-                
-            }];
+            }
+
         }
             break;
         case FBSessionStateClosed:
@@ -69,6 +73,34 @@
         [alertView show];
     }    
 }
+
+
++ (void)uploadPhotoToFacebook:(UIImage *)image {
+    
+    NSMutableDictionary *params = [[NSMutableDictionary alloc] initWithObjectsAndKeys:UIImagePNGRepresentation(image), @"picture", nil];
+    [FBRequestConnection startWithGraphPath:@"me/photos"
+                                 parameters:params
+                                 HTTPMethod:@"POST"
+                          completionHandler:^(FBRequestConnection *connection,
+                                              id result,
+                                              NSError *error)
+     {
+         NSString *alertText;
+         if (error) {
+             alertText = [NSString stringWithFormat:
+                          @"error: domain = %@, code = %d",
+                          error.domain, error.code];
+         } else {
+             alertText = [NSString stringWithFormat:
+                          @"Posted action, id: %@",
+                          [result objectForKey:@"id"]];
+         }
+         ALog(@"Upload failure with %@", alertText);
+         ALog(@"%@", error);
+     }];
+    
+}
+
 
 
 @end
