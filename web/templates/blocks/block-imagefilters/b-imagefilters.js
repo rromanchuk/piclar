99165a43b454c5bbf38b6a68ccf159065c979312
Max Degterev <max@ostrovok.ru>
2012-11-18 18:24:38 +0400

    FCUK

--- web/templates/blocks/block-imagefilters/b-imagefilters.js
+++ web/templates/blocks/block-imagefilters/b-imagefilters.js
@@ -2,18 +2,14 @@
 
 (function($){
 S.blockImageFilters = function(settings) {
-    this.options = $.extend({
-    }, settings);
+    this.options = $.extend({}, settings);
 
     this.els = {};
 };
 
 S.blockImageFilters.prototype.init = function() {
     this.els.block = $('.b-imagefilters');
-    this.els.canvas = this.els.block.find('.b-i-canvas');
-
-    this.canvas = this.els.canvas[0];
-    this.ctx = this.canvas.getContext('2d');
+    this.els.filtered = this.els.block.find('.b-i-filtered')
 
     this.els.filters = this.els.block.find('.b-i-filterlist');
 
@@ -34,30 +30,85 @@ S.blockImageFilters.prototype.setImage = function(settings) {
 
     var opt = $.extend({
         cx: 0,
-        cy: 0
+        cy: 0,
+        resize: false,
+        width: 640,
+        height: 640,
+        scale: 0
     }, settings);
 
-    this.ctx.drawImage(opt.image, opt.cx, opt.cx, 640, 640, 0, 0, 640, 640);
+    var that = this,
+        image = $(opt.image).clone();
+
+    this.els.filtered.append(image);
 
     this.active && (delete this.caman);
 
-    this.caman = Caman(this.canvas, function() {});
-    var that = this;
-    $(window).on('load', function() {
-        setTimeout(function(){
-            that.caman.sepia(140).render();
-        }, 3000);
-    });
-    
+    this.deactivate();
 
-    this.active = true;
+    this.caman = Caman(image[0], function() {
+        this.crop(opt.width, opt.height, opt.cx, opt.cy);
+        that.activate();
+    });
+
+    $.pub('b_imagefilters_imageset');
 };
 
+S.blockImageFilters.prototype.activate = function() {
+    this.els.block.addClass('active');
+
+    this.active = true;
+
+    $.pub('b_imagefilters_activated');
+};
+S.blockImageFilters.prototype.deactivate = function() {
+    this.els.block.removeClass('active');
+
+    this.active = true;
+
+    $.pub('b_imagefilters_deactivated');
+};
+
+S.blockImageFilters.prototype.filters = {
+    'bnw': function() {
+        this.greyscale();
+    },
+    'sepia': function() {
+        this.sepia(70);
+    },
+    'saturation': function() {
+        this.saturation(90);
+    },
+    'blur': function() {
+        this.radialBlur();
+    },
+    'noise': function() {
+        this.noise(23);
+    }
+};
+
+
 S.blockImageFilters.prototype.logic = function() {
-    var that = this;
+    var that = this,
+        type;
+
+    var activate = function() {
+        that.activate();
+    };
+    var applyFilter = function() {
+        that.filters[type].call(that.caman);
+        that.caman.render(activate);
+    };
 
     var handleFilterClick = function() {
         if (!that.active) return;
+
+        type = this.getAttribute('data-filter');
+
+        if (typeof that.filters[type] === 'function') {
+            that.deactivate();
+            that.caman.revert.call(that.caman, applyFilter);
+        }
     };
 
     this.els.filters.on('click', '.b-i-filter', handleFilterClick);
--- web/templates/blocks/page-upload/p-upload.js
+++ web/templates/blocks/page-upload/p-upload.js
@@ -2,6 +2,17 @@
 // @require 'blocks/block-imagefilters/b-imagefilters.js'
 
 (function($) {
+    var page = S.DOM.content,
+
+        steps = page.find('.p-u-step'),
+        actions = page.find('.p-u-action');
+
+
+
+
+
+
+
     var crop = new S.blockImageCrop(),
         filters = new S.blockImageFilters();
 
@@ -16,6 +27,17 @@
 
     $.sub('b_imagecrop_ready', activateFilters);
 
+    steps.on('click', function() {
+        var el = $(this);
+
+        steps.filter('.active').removeClass('active');
+        actions.filter('.active').removeClass('active');
+
+        el.addClass('active');
+        actions.filter('[data-step="' + el.data('step') + '"]').addClass('active');
+    });
+
+    steps.eq(1).trigger('click');
 
     window.f = filters;
 })(jQuery);
