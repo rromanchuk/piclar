a64efe20f00964613a93b5b10daf1704a88e32b1
Lazarev Ivan <ivan.lazarev@ostrovok.ru>
2012-11-20 15:39:47 +0400

    fix migrations

--- web/feed/migrations/0001_initial.py
+++ web/feed/migrations/0001_initial.py
@@ -8,6 +8,145 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        pass
+        # Adding model 'FeedItem'
+        db.create_table('feed_feeditem', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('creator', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['person.Person'])),
+            ('shared', self.gf('ostrovok_common.pgarray.fields.IntArrayField')()),
+            ('liked', self.gf('ostrovok_common.pgarray.fields.IntArrayField')()),
+            ('data', self.gf('ostrovok_common.models.JSONField')()),
+            ('type', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('create_date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('modified_date', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+        ))
+        db.send_create_signal('feed', ['FeedItem'])
+
+        # Adding model 'FeedItemComment'
+        db.create_table('feed_feeditemcomment', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('item', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['feed.FeedItem'])),
+            ('create_date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('creator', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['person.Person'])),
+            ('comment', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal('feed', ['FeedItemComment'])
+
+        # Adding model 'FeedPersonItem'
+        db.create_table('feed_feedpersonitem', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('item', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['feed.FeedItem'])),
+            ('is_hidden', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('creator', self.gf('django.db.models.fields.related.ForeignKey')(related_name='+', to=orm['person.Person'])),
+            ('receiver', self.gf('django.db.models.fields.related.ForeignKey')(related_name='+', to=orm['person.Person'])),
+            ('create_date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal('feed', ['FeedPersonItem'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'FeedItem'
+        db.delete_table('feed_feeditem')
+
+        # Deleting model 'FeedItemComment'
+        db.delete_table('feed_feeditemcomment')
+
+        # Deleting model 'FeedPersonItem'
+        db.delete_table('feed_feedpersonitem')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '75'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'feed.feeditem': {
+            'Meta': {'object_name': 'FeedItem'},
+            'create_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['person.Person']"}),
+            'data': ('ostrovok_common.models.JSONField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'liked': ('ostrovok_common.pgarray.fields.IntArrayField', [], {}),
+            'modified_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'shared': ('ostrovok_common.pgarray.fields.IntArrayField', [], {}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'feed.feeditemcomment': {
+            'Meta': {'object_name': 'FeedItemComment'},
+            'comment': ('django.db.models.fields.TextField', [], {}),
+            'create_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['person.Person']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'item': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['feed.FeedItem']"})
+        },
+        'feed.feedpersonitem': {
+            'Meta': {'object_name': 'FeedPersonItem'},
+            'create_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'+'", 'to': "orm['person.Person']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_hidden': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'item': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['feed.FeedItem']"}),
+            'receiver': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'+'", 'to': "orm['person.Person']"})
+        },
+        'person.person': {
+            'Meta': {'object_name': 'Person'},
+            'birthday': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'checkins_count': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'create_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'null': 'True', 'blank': 'True'}),
+            'firstname': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'followers': ('ostrovok_common.pgarray.fields.IntArrayField', [], {}),
+            'following': ('ostrovok_common.pgarray.fields.IntArrayField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_email_verified': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'lastname': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'location': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'moderated_by': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['person.Person']", 'null': 'True', 'blank': 'True'}),
+            'moderated_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'modified_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'photo': ('django.db.models.fields.files.ImageField', [], {'max_length': '2048', 'db_index': 'True'}),
+            'settings': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['person.PersonSetting']", 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'sex': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'status': ('django.db.models.fields.IntegerField', [], {'default': '2'}),
+            'token': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True'})
+        },
+        'person.personsetting': {
+            'Meta': {'object_name': 'PersonSetting'},
+            'data': ('ostrovok_common.models.JSONField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        }
+    }
 
     complete_apps = ['feed']
