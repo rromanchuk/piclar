3de36a5b10b72cb0dfec2958c80c00fc2279f01f
Ryan Romanchuk <ryan@ostrovok.ru>
2012-12-10 14:02:05 +0400

    do not give up until accuracy of 100 meters is reached instead of 1km which is ridiculous

--- ios/social/Location.m
+++ ios/social/Location.m
@@ -17,7 +17,7 @@
     if (self) {
         self.locationManager                 = [[CLLocationManager alloc] init];
         self.locationManager.delegate        = self;
-        self.locationManager.desiredAccuracy = kCLLocationAccuracyKilometer;
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters;
         self.locationManager.distanceFilter  = 250;
         self.locationManager.purpose         = NSLocalizedString(@"LOCATION_EXPLANATION", @"Explain to the user why we need location");
         self.geoCoder = [[CLGeocoder alloc] init];
