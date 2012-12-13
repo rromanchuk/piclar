45a8789bc1c2fbb01d3e5fc0c589c621a343fd0f
Ryan Romanchuk <ryan@ostrovok.ru>
2012-12-13 21:13:31 +0400

    helper to add exif data back into nsdata representation

---
+++ ios/social/categories/NSData+Exif.h
@@ -0,0 +1,14 @@
+//
+//  NSData+Exif.h
+//  Ostronaut
+//
+//  Created by Ryan Romanchuk on 12/13/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (Exif)
+- (NSMutableData *)addExifData;
+
+@end
