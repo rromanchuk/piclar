0e29fbd4f89e0b54b15bf025044e2b0d59e73581
Lazarev Ivan <ivan.lazarev@ostrovok.ru>
2013-01-15 20:59:04 +0400

    add signature check

--- web/api/v1/game_api.py
+++ web/api/v1/game_api.py
@@ -2,7 +2,10 @@ from utils import  filter_fields, AuthTokenMixin, doesnotexist_to_404, date_in_w
 from base import *
 from game.models import Score
 from base64 import b64decode
+
 import json
+from hashlib import md5
+
 
 class ScoreGetPost(ApiMethod,CommonRefineMixin):
     def get(self):
@@ -11,8 +14,11 @@ class ScoreGetPost(ApiMethod,CommonRefineMixin):
     def post(self):
         data = self.request.POST.get('data')
         sign = self.request.POST.get('signature')
+
+        if sign <> md5(data).hexdigest():
+            return self.error(status=401, message='unauthorized')
         if not data or not sign:
-            return self.error(message="incorrecet params")
+            return self.error(message="incorrect params")
 
         data = json.loads(b64decode(data))
         Score.objects.create_score(data.get('name'), data.get('score'))
