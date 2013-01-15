e4924155647bee79a14504f866fca7580c9147fe
Lazarev Ivan <ivan.lazarev@ostrovok.ru>
2013-01-15 17:57:36 +0400

    change game api

--- web/api/v1/game_api.py
+++ web/api/v1/game_api.py
@@ -1,10 +1,20 @@
 from utils import  filter_fields, AuthTokenMixin, doesnotexist_to_404, date_in_words, CommonRefineMixin
 from base import *
 from game.models import Score
+from base64 import b64decode
+import json
 
 class ScoreGetPost(ApiMethod,CommonRefineMixin):
     def get(self):
         return [{'name' : score.name, 'score': score.score} for score in Score.objects.get_high_score()]
+
     def post(self):
-        Score.objects.create_score(self.request.POST.get('name'), self.request.POST.get('score'))
-        return {}
+        data = self.request.POST.get('data')
+        sign = self.request.POST.get('signature')
+        if not data or not sign:
+            return self.error(message="incorrecet params")
+
+        data = json.loads(b64decode(data))
+        Score.objects.create_score(data.get('name'), data.get('score'))
+        return self.get()
+
