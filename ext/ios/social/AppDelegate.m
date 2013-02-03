0db3cbb47aba50c652448701630c10223dd47aaf
Ryan Romanchuk <ryan@ostrovok.ru>
2013-02-04 02:00:04 +0400

    update crashalytics

--- ext/ios/social/AppDelegate.m
+++ ext/ios/social/AppDelegate.m
@@ -40,7 +40,7 @@
     NSMutableDictionary *takeOffOptions = [[NSMutableDictionary alloc] init];
     [takeOffOptions setValue:launchOptions forKey:UAirshipTakeOffOptionsLaunchOptionsKey];
     
-    [Crashlytics startWithAPIKey:@"b57451c4dba3f37272bceefeecefd74df4c051d6"];
+    [Crashlytics startWithAPIKey:@"cbbca2d940f872c4617ddb67cf20ec9844d036ea"];
     // Create Airship singleton that's used to talk to Urban Airship servers.
     // Please populate AirshipConfig.plist with your info from http://go.urbanairship.com
     [UAirship takeOff:takeOffOptions];
