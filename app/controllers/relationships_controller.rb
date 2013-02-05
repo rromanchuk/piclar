f7c62dcf0f31996e82b3f10e66445c3228c68a04
Ryan Romanchuk <rromanchuk@gmail.com>
2013-02-05 22:37:04 +0400

    missed

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,17 @@
+class RelationshipsController < ApplicationController
+  before_filter :authenticate_user!
+  respond_to :json
+
+  def create
+    @user = User.find(params[:relationship][:followed_id])
+    current_user.follow!(@user)
+    render "users/show"
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    current_user.unfollow!(@user)
+    render "users/show"
+  end
+
+end
