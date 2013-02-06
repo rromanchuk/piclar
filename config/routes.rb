3b5074a7f752782ada7a484367f6761937bdccb9
Ryan Romanchuk <rromanchuk@gmail.com>
2013-02-06 23:13:30 +0400

    spelling

--- config/routes.rb
+++ config/routes.rb
@@ -91,7 +91,7 @@ Ostronaut::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'static#commingsoon'
+  root :to => 'static#comingsoon'
 
   # See how all your routes lay out with "rake routes"
 
