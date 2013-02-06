7b4f0105ab609dcbb4efcc30aaa191fa0fc58646
Ryan Romanchuk <rromanchuk@gmail.com>
2013-02-06 21:05:01 +0400

    we don't actually have these routes yet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,22 +2,22 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 
-  unless Rails.application.config.consider_all_requests_local
-    rescue_from Exception,                            :with => :render_error
-    rescue_from ActiveRecord::RecordNotFound,         :with => :render_not_found
-    rescue_from ActionController::RoutingError,       :with => :render_not_found
-    rescue_from ActionController::UnknownController,  :with => :render_not_found
-    rescue_from ActionController::UnknownAction,      :with => :render_not_found
-  end
+  # unless Rails.application.config.consider_all_requests_local
+  #   rescue_from Exception,                            :with => :render_error
+  #   rescue_from ActiveRecord::RecordNotFound,         :with => :render_not_found
+  #   rescue_from ActionController::RoutingError,       :with => :render_not_found
+  #   rescue_from ActionController::UnknownController,  :with => :render_not_found
+  #   rescue_from ActionController::UnknownAction,      :with => :render_not_found
+  # end
 
-  private
-  def render_not_found(exception)
-    render :template => 'error_pages/404', :layout => false, :status => 404
-  end
+  # private
+  # def render_not_found(exception)
+  #   render :template => 'error_pages/404', :layout => false, :status => 404
+  # end
 
-  def render_error(exception)
-    render :template => 'error_pages/505', :layout => false, :status => 500
-  end
+  # def render_error(exception)
+  #   render :template => 'error_pages/505', :layout => false, :status => 500
+  # end
 
 
 end
