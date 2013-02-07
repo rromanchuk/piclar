1e5813bdb2094ffe14caaf7c52e4d1e2c56e3a51
Ryan Romanchuk <rromanchuk@gmail.com>
2013-02-07 23:38:17 +0400

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130206220539) do
+ActiveRecord::Schema.define(:version => 20130207192902) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(:version => 20130206220539) do
     t.integer  "receiver_id"
     t.boolean  "is_read",           :default => false
     t.string   "notification_type"
-    t.boolean  "is_active"
+    t.boolean  "is_active",         :default => true
     t.datetime "created_at",                           :null => false
     t.datetime "updated_at",                           :null => false
   end
